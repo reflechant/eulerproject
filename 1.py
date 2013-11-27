e16913dd8c25e1740ba9748679c576b16c0955f5
reflechant <reflechant@users.noreply.github.com>
2013-11-27 13:00:17 +0400

    ex.1 improved

--- 1.py
+++ 1.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 
-a = [x for x in xrange(1000) if not x%3]
-b = [x for x in xrange(1000) if not x%5]
-c = a
-for x in b:
-	if x not in c:
-		c.append(x)
-
-print sum(c)
+a = (x for x in xrange(1000) if not x%3 or not x%5)
+print sum(a)
 
