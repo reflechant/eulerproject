c31bc47d75fb6f98faad5c3fa801a47cb16adefa
reflechant <reflechant@users.noreply.github.com>
2013-11-29 13:25:59 +0400

    ex.2 pep8 checked

--- 2.py
+++ 2.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
+
 def fibonacci(max):
-	a,b=0,1
-	while True:
-		c=a+b
-		a,b=b,c
-		if c<=max:
-			if not c%2:
-				yield c
-		else: raise StopIteration
+    a, b = 0, 1
+    while True:
+        c = a + b
+        a, b = b, c
+        if c <= max:
+            if not c % 2:
+                yield c
+        else:
+            raise StopIteration
 
 num = fibonacci(4e6)
 print sum(num)
