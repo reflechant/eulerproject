c4519f69137bddd4ed8bd93cc8c979bae01c04cc
reflechant <reflechant@users.noreply.github.com>
2013-11-29 13:21:00 +0400

    ex.1 pep8 checked

--- 1.py
+++ 1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
 
-a = (x for x in xrange(1000) if not x%3 or not x%5)
+a = (x for x in xrange(1000) if not x % 3 or not x % 5)
 print sum(a)
-
--- 3.py
+++ 3.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
+from math import sqrt
+
 def prime(max):
     prime=[2,]
-    for x in xrange(3,max+1):
+    for x in xrange( 3, int(sqrt(max)) ):
         for y in prime:
             if x%y:
                 prime.append(x)
-                #break
+                break
 	return prime
 
 print prime(100)
