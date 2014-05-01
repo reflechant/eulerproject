23343374dd5949898a40286933890a98d1e762c3
reflechant <reflechant@users.noreply.github.com>
2014-05-01 16:20:42 +0400

    ex.3 done, pep8

--- 3.py
+++ 3.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
-from math import sqrt
 
 def prime(max):
-    prime=[1,]              # 1 is a universal prime factor
-    limit=int(sqrt(max))    # there are no prime factors beyond sqrt(max)
-    x = 1           
-    while x <= limit:
-        for y in prime:
-            if x%y:
-                prime.append(x)
-                break
-        x = x + 1
-	return prime
+    prime = [1, ]            # 1 is universal prime factor
+    x = 2
+    while x <= max:
+        if not max % x:       # if max is divisible by x
+            prime.append(x)   # save x as one of max' prime factors
+        while not max % x:    # prevent adding x^2 and x^3 to prime
+            max = max / x
 
-print prime(100)
+        x = x + 1
+
+    return prime
+print prime(600851475143)
