7cb79eccb15ee5e67db31b6d4f17faf324be3f4a
reflechant <reflechant@users.noreply.github.com>
2014-05-01 15:33:16 +0400

    ex.3 updated

--- 3.py
+++ 3.py
@@ -3,12 +3,15 @@
 from math import sqrt
 
 def prime(max):
-    prime=[2,]
-    for x in xrange( 3, int(sqrt(max)) ):
+    prime=[1,]              # 1 is a universal prime factor
+    limit=int(sqrt(max))    # there are no prime factors beyond sqrt(max)
+    x = 1           
+    while x <= limit:
         for y in prime:
             if x%y:
                 prime.append(x)
                 break
+        x = x + 1
 	return prime
 
 print prime(100)
